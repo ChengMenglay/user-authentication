d9f33e1c4f5b533215bf586482af6d2e98094e77
Menglay <cheng.menglay79@gmail.com>
2024-05-25 12:58:43 +0700

    initial commit

--- user_authentication/index.js
+++ user_authentication/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 app.use(express.json());
+dotenv.config({ path: "./.env" });
 const cors = require("cors");
 app.use(cors());
 app.listen(8080, () => {
-  console.log("Server Started on Port 8080");
+  console.log("Server Started on Port " + 8080);
 });
 
-const user = require("../router/user.router");
+const user = require("./router/user.router");
 user(app);
